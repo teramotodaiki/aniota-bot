1276191b49e96a9ee6ffc6a149510fdb966b8cbf
teramotodaiki <i101323@gmail.com>
2018-12-30 00:42:54 +0900

    feature: カテゴリで弾く

--- src/cron.ts
+++ src/cron.ts
@@ -2,6 +2,13 @@ import * as functions from 'firebase-functions'
 import * as syobocal from './syobocal'
 import firestore, { SyobocalTitle, Timestamp } from './firestore'
 import * as moment from 'moment'
+import { includes } from 'lodash'
+
+const categoryFilter = [
+  syobocal.TitleCategory.アニメ,
+  syobocal.TitleCategory.OVA,
+  syobocal.TitleCategory.映画
+] // TODO: 要検討
 
 export const cron = functions.https.onRequest(async (request, response) => {
   console.log('start')
@@ -17,15 +24,15 @@ export const cron = functions.https.onRequest(async (request, response) => {
   for (const item of result) {
     const ref = firestore.doc(`syobocal_titles/${item.tid}`)
     const current = await ref.get()
-    if (!current.exists) {
-      // id=tid が存在しなければ追加
-      const title: SyobocalTitle = {
-        ...item,
-        created_at: Timestamp.now(),
-        updated_at: null
-      }
-      batch.create(ref, title)
+    if (current.exists) continue // 既に存在する
+    if (!includes(categoryFilter, item.cat)) continue // カテゴリで弾く
+    // id=tid が存在しなければ追加
+    const title: SyobocalTitle = {
+      ...item,
+      created_at: Timestamp.now(),
+      updated_at: null
     }
+    batch.create(ref, title)
   }
   await batch.commit()
   return response.status(200)
